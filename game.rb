e29e35ca71ec36ecf221ad97192186a4c55ad26f
LIZXP <Peter.xli@outlook.com>
2022-10-26 11:08:55 -0600

    defined new_turn method to game.rb and loop it again if user lives greater than 0

--- game.rb
+++ game.rb
@@ -5,4 +5,23 @@ class Game
     @player1 = Player.new("player1")
     @player2 = Player.new("player2")
   end
+
+  def start
+    puts "New Game Started. Welcome #{@player1.name} and #{@player2.name}"
+    p @player1.name
+    p @player2.name
+    newturn()
+  end
+
+  def newturn
+    @player1.new_question
+    @player2.new_question
+    if @player1.lives > 0 && @player2.lives > 0
+      puts '---------NEW-TURN---------'
+      newturn()
+    else
+      puts '---------GAME-OVER--------'
+      puts 'Good bye!'
+    end
+  end
 end
--- player.rb
+++ player.rb
@@ -5,4 +5,20 @@ class Player
     @name = name
     @lives = 3
   end
+
+  def take_live
+    @lives -= 1
+  end
+
+  def new_question
+    new_question = Question.new
+    puts "#{name}: What does #{new_question.num1} plus #{new_question.num2} equal?"
+    @userinput = gets.chomp
+    if new_question.check_answer(@userinput.to_i)
+      puts "Well Done! Your Right"
+    else
+      puts "Seriously? No!"
+      take_live()
+    end
+  end
 end
