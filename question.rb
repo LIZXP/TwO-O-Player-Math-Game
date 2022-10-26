3ca7ef46237a7ff3e64833409c768347bf12c9e3
LIZXP <Peter.xli@outlook.com>
2022-10-25 22:49:00 -0600

    add check_answer method in question class

--- question.rb
+++ question.rb
@@ -5,4 +5,12 @@ class Question
     @num2 = rand(1..30)
     @total = num1 + num2
   end
+
+  def check_answer(input)
+    if @total === input
+      return true
+    else
+      return false
+    end
+  end
 end
